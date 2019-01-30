7492b2c18088f760c5eacf89e46cef7339fea1af
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-01-30 12:37:13 -0800

    server up and running

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
+//to start a server file, you _must_ require express
+var express = require('express');
+
+//set your port name, will be used in app.listen
+var port = 3000;
+
+//invoke express
+var app = express();
 
 
+
+//actually have the server running
+app.listen(port,function(){
+    console.log("Server Is up and running!");
+   });
+
+//exporting our functions elsewhere
+module.exports = function (app) {
+    // do something with app
+  };
