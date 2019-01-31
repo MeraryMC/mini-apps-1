373fa345ced9bfbe337533e0aa07616d29398a16
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-01-30 16:42:53 -0800

    returned to react-less implementation and continued working on server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,11 @@
 //to start a server file, you _must_ require express
 var express = require('express');
 
+var path = require('path');
+
+//need body parser to handle our inputs
+var bodyParser = require('body-parser');
+
 //set your port name, will be used in app.listen
 var port = 8080;
 
@@ -8,11 +13,18 @@ var port = 8080;
 var app = express();
 
 
+//tell express where to start serving up the files, will 
+//look for index.html file by itself!
+app.use(express.static(path.join(__dirname, 'client')));
+
+
 
 //actually have the server running
 app.listen(port,function(){
     console.log("Server Is up and running!");
    });
+
+
 
 //exporting our functions elsewhere
 module.exports = function (app) {
