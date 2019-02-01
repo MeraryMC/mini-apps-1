d874f69dbe41eee2e970b39f8ff4940964765a05
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-01-31 17:52:28 -0800

    react implementation underway

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,31 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { items: [], text: ''};
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleCheckout = this.handleCheckout.bind(this);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Checkout</h1>
+        <form onSubmit={this.handleCheckout}>
+          <label htmlFor="Checkout">
+            Ready for Checkout?
+          </label>
+          <button>
+            Go to Checkout
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+// class F1 extends React.Component
 
 
+//this should always be at the very bottom
+ReactDOM.render(<App/ >, document.getElementById('app'));
